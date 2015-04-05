a4abfe693813d2d40316bf4e20b9f0eef6c87a01
Theo Tosini <theo.tosini@gmail.com>
2015-04-04 23:55:25 -0400

    Improve PEP8 compatibility, so that my editor does not yell at me

--- ctfapp/models.py
+++ ctfapp/models.py
@@ -4,21 +4,23 @@ from datetime import datetime
 
 import pickle
 
+
 class Problem(models.Model):
-	problem_title = models.CharField(max_length=200)
-	problem_text = models.CharField(max_length=500)
-	problem_value = models.IntegerField()
-	problem_category = models.CharField(max_length=30)
-	hint_text = models.CharField(max_length=200)
-	flag_sha512_hash = models.CharField(max_length=128)
+    problem_title = models.CharField(max_length=200)
+    problem_text = models.CharField(max_length=500)
+    problem_value = models.IntegerField()
+    problem_category = models.CharField(max_length=30)
+    hint_text = models.CharField(max_length=200)
+    flag_sha512_hash = models.CharField(max_length=128)
+
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
-	
-	school = models.CharField(max_length=100)
+    user = models.OneToOneField(User)
 
-	participating = models.BooleanField(default=True)
-	solved = models.BinaryField(default=pickle.dumps({}))
-	
-	score = models.IntegerField(default=0)
-	score_lastupdate = models.DateTimeField(default=datetime.now())
+    school = models.CharField(max_length=100)
+
+    participating = models.BooleanField(default=True)
+    solved = models.BinaryField(default=pickle.dumps({}))
+
+    score = models.IntegerField(default=0)
+    score_lastupdate = models.DateTimeField(default=datetime.now())
--- ctfapp/views.py
+++ ctfapp/views.py
@@ -10,57 +10,60 @@ import hashlib
 import pickle
 from datetime import datetime
 
+
 def index(request):
-	return render_to_response('index.html', {
-		'user': request.user,
-	})
+    return render_to_response('index.html', {
+        'user': request.user,
+    })
+
 
 def scoreboard(request):
-	user_list = UserProfile.objects.all().order_by('-score', 'score_lastupdate')
-	return render(request, 'scoreboard.html', {
-		'user': request.user,
-		'user_list': user_list,
-	})
+    user_list = UserProfile.objects.all().order_by('-score', 'score_lastupdate')
+    return render(request, 'scoreboard.html', {
+        'user': request.user,
+        'user_list': user_list,
+    })
+
 
 @login_required
 def problems(request):
-	last_submission = -1
-	status = ""
-	
-	solved = pickle.loads(request.user.userprofile.solved)
-	
-	if request.method == 'POST':
-		form = SubmitForm(request.POST)
+    last_submission = -1
+    status = ""
 
-		if form.is_valid():
-			guess = form.cleaned_data["flag_guess"]
-			pid = form.cleaned_data["problem"]
-			last_submission = pid
-			
-			problem = Problem.objects.get(id=pid)
-			good = hashlib.sha512(guess.encode()).hexdigest() == problem.flag_sha512_hash
-			
-			next_count = solved[pid][1]+1 if pid in solved else 1
-			if pid in solved and solved[pid][0]:
-				status = "ALREADY"
-			elif good:
-				solved[pid] = (True, next_count)
-				request.user.userprofile.score += problem.problem_value
-				request.user.userprofile.score_lastupdate = datetime.now()
-				status = "SOLVED"
-			else:
-				solved[pid] = (False, next_count)
-				status = "FAILED"
-			
-			request.user.userprofile.solved = pickle.dumps(solved)
-			request.user.userprofile.save()
-		
-	problem_list = Problem.objects.all()
-	return render(request, 'problems.html', {
-		'user': request.user,
-		'problem_list': problem_list,
-		'form': SubmitForm(),
-		'last': last_submission,
-		'status': status,
-		'solved': solved,
-	})
+    solved = pickle.loads(request.user.userprofile.solved)
+
+    if request.method == 'POST':
+        form = SubmitForm(request.POST)
+
+        if form.is_valid():
+            guess = form.cleaned_data["flag_guess"]
+            pid = form.cleaned_data["problem"]
+            last_submission = pid
+
+            problem = Problem.objects.get(id=pid)
+            good = hashlib.sha512(guess.encode()).hexdigest() == problem.flag_sha512_hash
+
+            next_count = solved[pid][1] + 1 if pid in solved else 1
+            if pid in solved and solved[pid][0]:
+                status = "ALREADY"
+            elif good:
+                solved[pid] = (True, next_count)
+                request.user.userprofile.score += problem.problem_value
+                request.user.userprofile.score_lastupdate = datetime.now()
+                status = "SOLVED"
+            else:
+                solved[pid] = (False, next_count)
+                status = "FAILED"
+
+            request.user.userprofile.solved = pickle.dumps(solved)
+            request.user.userprofile.save()
+
+    problem_list = Problem.objects.all()
+    return render(request, 'problems.html', {
+        'user': request.user,
+        'problem_list': problem_list,
+        'form': SubmitForm(),
+        'last': last_submission,
+        'status': status,
+        'solved': solved,
+    })
