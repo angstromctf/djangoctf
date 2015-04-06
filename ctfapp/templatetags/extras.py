32d790f17da147472114bf18d6d36cb19af55104
Artemis Tosini <me@artem.ist>
2015-04-06 17:06:16 -0400

    Added profiles, which show what each team has solved

--- ctfapp/templatetags/extras.py
+++ ctfapp/templatetags/extras.py
@@ -3,19 +3,38 @@ from ctfapp.models import UserProfile
 
 register = template.Library()
 
+
 @register.filter
 def is_solved(problem, solved):
-	return (problem.id in solved) and solved[problem.id][0]
+    return (problem.id in solved) and solved[problem.id][0]
+
 
 @register.filter
 def try_count(problem, solved):
-	return solved[problem.id][1] if problem.id in solved else 1
+    return solved[problem.id][1] if problem.id in solved else 1
+
 
 @register.filter
 def place(user):
-	for index, item in enumerate(UserProfile.objects.all().order_by('-score', 'score_lastupdate')):
-		if item.user.id == user.id:
-			return index+1
-	
-	return -1
-	
+    for index, item in enumerate(UserProfile.objects.all().order_by('-score', 'score_lastupdate')):
+        if item.user.id == user.id:
+            return index+1
+
+    return -1
+
+
+@register.filter
+def possession(name):
+    if name[-1] == 's':
+        return name + '\''
+    return name + '\'s'
+
+
+@register.filter
+def solved(value):
+    return "Solved" if value else "Unsolved"
+
+
+@register.filter
+def color_solved(value):
+    return 'green' if value else 'red'
