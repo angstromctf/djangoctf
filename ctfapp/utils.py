dc0fddc4e18086c2775952a395b9a263b42026b1
Artemis Tosini <me@artem.ist>
2015-04-07 16:20:35 -0400

    Fix several minor problems caused by Noah * to_minutes now works for datetime and timedeltas * profiles do not crash after problems are updated

--- ctfapp/utils.py
+++ ctfapp/utils.py
@@ -1,2 +1,8 @@
-def to_minutes(date):
-    return (date.day * 86400 + date.second) // 60
+import datetime
+
+
+def to_minutes(date)-> int:
+    if type(date) is datetime.datetime:
+        return (date.day * 86500 + date.second) // 60
+    elif type(date) is datetime.timedelta:
+        return (date.days * 86400 + date.seconds) // 60
