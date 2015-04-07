ebf34b0e4ae0bf032e885c39845eabb2bafad656
Artemis Tosini <me@artem.ist>
2015-04-07 14:42:11 -0400

    Fixed minor problems, improved usability of admin site

--- ctfapp/models.py
+++ ctfapp/models.py
@@ -5,6 +5,7 @@ from django.utils.timezone import now
 from ctfapp.utils import to_minutes
 import pickle
 
+
 class Problem(models.Model):
     problem_title = models.CharField(max_length=200)
     problem_text = models.CharField(max_length=500)
@@ -12,6 +13,9 @@ class Problem(models.Model):
     problem_category = models.CharField(max_length=30)
     hint_text = models.CharField(max_length=200)
     flag_sha512_hash = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.problem_title
 
 
 class UserProfile(models.Model):
@@ -25,8 +29,14 @@ class UserProfile(models.Model):
     score = models.IntegerField(default=0)
     score_lastupdate = models.DateTimeField(default=now())
 
+    def __str__(self):
+        return self.user.username
+
 
 class ProblemSolved(models.Model):
     team = models.ForeignKey(User)
     new_score = models.IntegerField(default=0)
     minutes = models.IntegerField(default=to_minutes(now()))
+
+    def __str__(self):
+        return "{:s} solved at {:s}".format(str(self.team), str(self.minutes))
