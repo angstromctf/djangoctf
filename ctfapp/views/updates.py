459709664404ddc38f2d8760583914ac51d2d61d
singerng <singerng@gmail.com>
2015-05-20 15:43:52 -0400

    I'll really use my computer later

--- ctfapp/views/updates.py
+++ ctfapp/views/updates.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils import timezone
 
-from ctfapp.models import updates
+from ctfapp.models import Update
 
 def updates(request: HttpRequest):
     """
