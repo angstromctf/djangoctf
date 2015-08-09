a1849e3d409d559a79e2e418cdea860e2d03d330
Noah Singer <singerng@gmail.com>
2015-08-09 16:04:15 -0400

    Updated the importer script to use JSON and new static links

--- importer.py
+++ importer.py
@@ -3,12 +3,13 @@ from django.conf import settings
 from django.core.exceptions import MultipleObjectsReturned
 from djangoctf.settings import DATABASES, TIME_ZONE
 
-from sys import argv
 from os import listdir, makedirs
 from os.path import isdir, isfile, realpath
 from hashlib import sha512
 from shutil import copyfile, rmtree
 import argparse
+import json
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("problems_directory", help="import problems from directory")
@@ -32,6 +33,8 @@ if args.reset:
 if args.reset_static:
     rmtree(STATIC)
 
+pattern = re.compile('\{\{([^,\}]*),([^\}]*)\}\}')
+
 for category in listdir(path):
     category_path = path + '/' + category
     if not isdir(category_path) or category[0] == '.':
@@ -42,45 +45,41 @@ for category in listdir(path):
         if not isdir(problem_path):
             continue
 
-        # Check if there's a special name file
         try:
-            name = open(problem_path + '/name.txt').read()
-        except FileNotFoundError:
-            name = problem
-
-        # Get the problem text and hint
-        try:
-            text = open(problem_path + '/problem.txt').read().strip()
-            hint = open(problem_path + '/hint.txt').read().strip()
-            value = int(open(problem_path + '/value.txt').read())
-            flag = sha512(open(problem_path + '/flag.txt').read().strip().encode('utf-8')).hexdigest()
+            with open(problem_path + '/problem.json') as data_file:
+                data = json.load(data_file)
         except FileNotFoundError:
             print("Error: Failed to import problem {:s}/{:s}".format(category, problem))
             continue
 
-        if Problem.objects.filter(problem_title=name).exists():
+        data["text"] = pattern.sub("<a href=\"/static/problems/" + category + "/" + problem +
+                                   "/\\1\" target=\"_blank\">\\2</a>", data["text"])
+        data["hint"] = pattern.sub("<a href=\"/static/problems/" + category + "/" + problem +
+                                   "/\\1\" target=\"_blank\">\\2</a>", data["hint"])
+
+        if Problem.objects.filter(problem_title=data["name"]).exists():
             # Update the problem if it already exists
             try:
-                problem_obj = Problem.objects.get(problem_title=name)
-                problem_obj.problem_text = text
-                problem_obj.hint_text = hint
-                problem_obj.problem_value = value
-                problem_obj.flag_sha512_hash = flag
+                problem_obj = Problem.objects.get(problem_title=data["name"])
+                problem_obj.problem_text = data["text"]
+                problem_obj.hint_text = data["hint"]
+                problem_obj.problem_value = data["value"]
+                problem_obj.flag_sha512_hash = sha512(data["flag"].encode()).hexdigest()
                 # We can't update the name for obvious reasons
                 problem_obj.save()
 
             except MultipleObjectsReturned:
-                print("Error: Multiple problems exist with name {:s}".format(name))
+                print("Error: Multiple problems exist with name {:s}".format(data["name"]))
                 continue
             print("Note: Successfully updated problem {:s}/{:s}".format(category, problem))
         else:
             # Otherwise, create new problem
-            problem_obj = Problem(problem_title=name,
-                                  problem_text=text,
-                                  problem_value=value,
+            problem_obj = Problem(problem_title=data["name"],
+                                  problem_text=data["text"],
+                                  problem_value=data["value"],
                                   problem_category=category,
-                                  hint_text=hint,
-                                  flag_sha512_hash=flag)
+                                  hint_text=data["hint"],
+                                  flag_sha512_hash=sha512(data["flag"].encode()).hexdigest())
             problem_obj.save()
             print("Note: Successfully created problem {:s}/{:s}".format(category, problem))
 
@@ -103,14 +102,16 @@ for category in listdir(path):
         makedirs(static_dir, exist_ok=True)
 
         try:
-            files = open(problem_path + '/files.txt').readlines()
+            with open(problem_path + '/problem.json') as data_file:
+                data = json.load(data_file)
         except FileNotFoundError:
             continue
 
-        for file in files:
-            file_path = problem_path + '/' + file.strip()
-            static_path = static_dir + '/' + file.strip()
-            if len(file) == 0 or not isfile(file_path):
-                continue
-            copyfile(file_path, static_path)
-            print("Note: Copying static file {:s} for problem {:s}/{:s}".format(file.strip(), category, problem))
+        if "files" in data:
+            for file in data["files"]:
+                file_path = problem_path + '/' + file.strip()
+                static_path = static_dir + '/' + file.strip()
+                if len(file) == 0 or not isfile(file_path):
+                    continue
+                copyfile(file_path, static_path)
+                print("Note: Copying static file {:s} for problem {:s}/{:s}".format(file.strip(), category, problem))
