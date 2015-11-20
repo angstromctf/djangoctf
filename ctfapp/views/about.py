b49c7210f192c220533d235628cc8e03954a0c5f
kisaayano <kisa.ayano@icloud.com>
2015-11-20 09:28:49 -0500

    Update about page

--- ctfapp/views/about.py
+++ ctfapp/views/about.py
@@ -4,17 +4,18 @@ from random import shuffle
 
 people = [
     # Name               Picture          Grade Contributions
-    ['Noah Singer', 'noahsinger.jpg', 9, 'Platform and crypto problems'],
-    ['George Klees', 'georgeklees.jpg', 9, 'RE and binary problems'],
-    ['Artemis Tosini', 'theotosini.jpg', 9, 'Platform, deployment, problems'],
-    ['Andrew Komo', 'andrewkomo.jpg', 9, 'Many crypto problems'],
-    ['Aaron Szabo', 'aaronszabo.jpg', 11, '???'],
-    ['Akash Canjels', 'akashcanjels.jpg', 9, '???'],
-    ['Anthony Li', 'anthonyli.jpg', 11, 'Contest organizer'],
-    ['Daniel Chen', 'danielchen.jpg', 10, '???'],
-    ['Linden Yuan', 'lindenyuan.jpg', 10, '???'],
-    ['Noah Levine', 'noahlevine.jpg', 11, 'Contest organizer'],
-    ['Noah Kim', 'noahkim.jpg', 9, 'Website Design']
+    ['Noah Singer', 'noahsinger.jpg', 10, 'Platform and crypto problems, official team movitator'],
+    ['George Klees', 'georgeklees.jpg', 10, 'RE and binary problems'],
+    ['Artemis Tosini', 'theotosini.jpg', 10, 'Platform, deployment, problems'],
+    ['Andrew Komo', 'andrewkomo.jpg', 10, 'ALL THE CRYPTO'],
+    ['Arman Siddique', 'armansiddique.jpg', 10, 'Being god'],
+    ['Aaron Szabo', 'aaronszabo.jpg', 12, '???'],
+    ['Anthony Li', 'anthonyli.jpg', 12, 'Contest organizer'],
+    ['Daniel Chen', 'danielchen.jpg', 11, 'PR and Outreach'],
+    ['Chris Wang', 'chriswang.jpg', 10, 'Art, PR, and Outreach'],
+    ['Linden Yuan', 'lindenyuan.jpg', 11, '???'],
+    ['Noah Levine', 'noahlevine.jpg', 12, 'Contest organizer'],
+    ['Noah Kim', 'noahkim.jpg', 10, 'Website Design']
 ]
 PICTURE_STATIC = 'http://lorempixel.com/200/200/cats/'
 
