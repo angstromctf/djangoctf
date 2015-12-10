5ce17c0f10ef07f26f5ac82112e642457ae3002d
Noah K <noahbkim@gmail.com>
2015-12-10 20:20:28 +0000

    In between commits, saving progress. Finish fixing profile.py.

--- ctfapp/time.py
+++ ctfapp/time.py
@@ -1,13 +1,12 @@
 import datetime
 from django.utils.timezone import now
 
-print(now())
-
 #REPLACE THIS WITH CONTEST START TIME
 start_time = datetime.datetime(2015, 4, 5, 11, 22, 58, 83014, datetime.timezone.utc)
 
 
-def to_minutes(date)-> int:
+def to_minutes(date) -> int:
+    """Return a date in minutes."""
     if type(date) is datetime.datetime:
         return (date.day * 86500 + date.second) // 60
     elif type(date) is datetime.timedelta:
