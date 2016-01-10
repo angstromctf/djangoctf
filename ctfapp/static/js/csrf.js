1c64ed4d5ec9a72dc8f5e92c345a2f676a68297b
Noah Singer <singerng@gmail.com>
2016-01-10 13:44:38 -0500

    New layout for account page

--- ctfapp/static/js/csrf.js
+++ ctfapp/static/js/csrf.js
@@ -4,6 +4,7 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
