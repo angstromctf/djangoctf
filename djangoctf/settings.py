d30ae7b69ee60819c07af2b603398dadcef397ce
Noah Singer <singerng@gmail.com>
2016-01-22 01:51:40 -0500

    Problem sumission works!

--- djangoctf/settings.py
+++ djangoctf/settings.py
@@ -52,7 +52,21 @@ ROOT_URLCONF = 'djangoctf.urls'
 
 WSGI_APPLICATION = 'djangoctf.wsgi.application'
 
-TEMPLATES = []
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 TEMPLATE_CONTEXT_PROCESSORS = ["django.contrib.auth.context_processors.auth",
     "django.template.context_processors.debug",
@@ -61,6 +75,23 @@ TEMPLATE_CONTEXT_PROCESSORS = ["django.contrib.auth.context_processors.auth",
     "django.template.context_processors.static",
     "django.template.context_processors.tz",
     "django.contrib.messages.context_processors.messages"]
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
