970899d42672c873ba3cbbf0477d4fba3296e417
Noah Singer <singerng@gmail.com>
2016-01-23 22:35:32 -0500

    Requires POST on several views

--- ctfapp/views/change_password.py
+++ ctfapp/views/change_password.py
@@ -3,9 +3,11 @@ import json
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 
 @login_required
+@require_POST
 def change_password(request: HttpRequest):
     """
     View for changing a password through AJAX.  Login is required.
@@ -14,32 +16,29 @@ def change_password(request: HttpRequest):
     alert_type = "danger"
     alert_class = "glyphicon glyphicon-remove-sign"
 
-    if request.method == "POST":
-        password = request.POST["old"]
+    password = request.POST["old"]
 
-        user = authenticate(username=request.user.get_username(), password=password)
+    user = authenticate(username=request.user.get_username(), password=password)
 
-        if user:
-            if request.POST["new"] == request.POST["confirm"]:
-                new = request.POST["new"]
+    if user:
+        if request.POST["new"] == request.POST["confirm"]:
+            new = request.POST["new"]
 
-                if new:
-                    user.set_password(new)
-                    user.save()
+            if new:
+                user.set_password(new)
+                user.save()
 
-                    login(request, user)
+                login(request, user)
 
-                    alert = "<strong>Yay!</strong> Password change successful!"
-                    alert_type = "success"
-                    alert_class = "glyphicon glyphicon-ok-sign"
-                else:
-                    alert = "<strong>Oops!</strong> Password may not be empty."
+                alert = "<strong>Yay!</strong> Password change successful!"
+                alert_type = "success"
+                alert_class = "glyphicon glyphicon-ok-sign"
             else:
-                alert = "<strong>Oops!</strong> Passwords must match."
+                alert = "<strong>Oops!</strong> Password may not be empty."
         else:
-            alert = "<strong>Oops!</strong> Incorrect password."
-
-        response_data = {"alert": alert, "alert_type": alert_type, "alert_class": alert_class}
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+            alert = "<strong>Oops!</strong> Passwords must match."
     else:
-        return HttpResponse("Must be POST.")
+        alert = "<strong>Oops!</strong> Incorrect password."
+
+    response_data = {"alert": alert, "alert_type": alert_type, "alert_class": alert_class}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
--- ctfapp/views/join_team.py
+++ ctfapp/views/join_team.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 from ctfapp.forms import ChangePasswordForm, CreateTeamForm, JoinTeamForm
 from ctfapp.models import Team
@@ -9,25 +10,22 @@ from ctfapp.decorators import team_required
 # Handle the HTTP request
 @login_required
 @team_required(invert=True)
+@require_POST
 def join_team(request: HttpRequest):
     """Create the account page."""
 
-    if request.method == 'POST':
-        join_team = JoinTeamForm(request.POST)
+    join_team = JoinTeamForm(request.POST)
 
-        if join_team.is_valid():
-            team = Team.objects.get(code=join_team.cleaned_data['code'])
-            team.user_count += 1
-            team.eligible = team.eligible and request.user.userprofile.eligible
+    if join_team.is_valid():
+        team = Team.objects.get(code=join_team.cleaned_data['code'])
+        team.user_count += 1
+        team.eligible = team.eligible and request.user.userprofile.eligible
 
-            team.save()
-            team.users.add(request.user)
+        team.save()
+        team.users.add(request.user)
 
-            print(type(request.user.userprofile))
-            request.user.userprofile.team = team
-            request.user.userprofile.save()
-    else:
-        join_team = JoinTeamForm()
+        request.user.userprofile.team = team
+        request.user.userprofile.save()
 
     return render(request, 'account.html', {'user': request.user,
                                             'change_password': ChangePasswordForm(),
