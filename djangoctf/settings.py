97ee23e7fe856b1b8ef26d7e5ac76c07ea8e72ef
Noah Singer <singerng@gmail.com>
2016-01-26 12:06:09 -0500

    Made small changes to settings

--- djangoctf/settings.py
+++ djangoctf/settings.py
@@ -67,23 +67,6 @@ TEMPLATES = [
     },
 ]
 
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
