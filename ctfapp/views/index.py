5508729e10c597e8ddf370833f4e42c0e5292414
kisaayano <kisa.ayano@icloud.com>
2016-03-07 22:32:41 -0500

    Pretty 404, moved things around in index, fixed a missing > in base.html

--- ctfapp/views/index.py
+++ ctfapp/views/index.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 from ctfapp.utils.time import contest_start, contest_end
 
+
 def index(request: HttpRequest):
-    """Create the index page."""
+    """Create the index page.
+    :param request: the HTTP Request
+    """
 
     return render(request, "index.html", {"user": request.user, "start": contest_start, "end": contest_end})
