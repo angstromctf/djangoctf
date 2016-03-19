7496d223000c584c653bc5e43e8068f1cf697e41
Noah Singer <singerng@gmail.com>
2016-03-19 14:04:15 -0400

    Fixed updates ordering

--- ctfapp/views/updates.py
+++ ctfapp/views/updates.py
@@ -11,7 +11,7 @@ def updates(request: HttpRequest):
     View for the updates page.
     """
 
-    updates_list = Update.objects.all().order_by('time')
+    updates_list = Update.objects.all().order_by('-time')
 
     return render(request, 'updates.html', {
         'user': request.user,
