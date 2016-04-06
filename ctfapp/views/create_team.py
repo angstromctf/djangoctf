5f414c16da25040262a2e944cb21f520486179c2
Noah Singer <singerng@gmail.com>
2016-04-05 22:25:31 -0400

    Testing is amazing

--- ctfapp/views/create_team.py
+++ ctfapp/views/create_team.py
@@ -71,8 +71,9 @@ def create_team(request: HttpRequest):
                     shell_password=shell_password,
                     code=code,
                     eligible=request.user.userprofile.eligible)
-        team.save()
+        
         team.users.add(request.user)
+        team.save()
 
         request.user.userprofile.team = team
         request.user.userprofile.save()
