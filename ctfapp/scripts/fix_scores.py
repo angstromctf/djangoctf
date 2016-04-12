37a6cb7b35d3b211862adcc1fd027cd69a29aea6
Noah Singer <singerng@gmail.com>
2016-04-11 21:35:07 -0400

    Fixed score fixer

--- ctfapp/scripts/fix_scores.py
+++ ctfapp/scripts/fix_scores.py
@@ -1,13 +1,18 @@
 from ctfapp.models import CorrectSubmission, Problem, Team
 
 def fix_scores():
+    i = 0
+
     for t in Team.objects.all():
         score = 0
 
-        for s in CorrectSubmission.objects.all().order_by('time'):
+        for s in CorrectSubmission.objects.all().filter(team=t).order_by('time'):
             score += s.problem.value
             s.new_score = score
             s.save()
 
         t.score = score
         t.save()
+
+        i += 1
+        print("Fixed score for team {%d}/{%d}" % (i, Team.objects.count()))
