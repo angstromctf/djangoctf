60c4888d583288de51e3adf3f5af5ef44770b6ba
Noah Singer <singerng@gmail.com>
2016-04-12 21:40:11 -0400

    Now updating the "solved" field

--- ctfapp/scripts/fix_scores.py
+++ ctfapp/scripts/fix_scores.py
@@ -6,10 +6,13 @@ def fix_scores():
     for t in Team.objects.all():
         score = 0
 
+        t.solved.clear()
+
         for s in CorrectSubmission.objects.all().filter(team=t).order_by('time'):
             score += s.problem.value
             s.new_score = score
             s.save()
+            t.solved.add(s.problem)
 
         t.score = score
         t.save()
