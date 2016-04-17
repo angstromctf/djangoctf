a06cf24495c7ff77bcabeb3f2a7ab89c7abeca7a
Noah Singer <singerng@gmail.com>
2016-04-17 00:07:37 -0400

    Locking after contest, oops

--- ctfapp/decorators.py
+++ ctfapp/decorators.py
@@ -2,7 +2,7 @@ from django.utils.decorators import available_attrs
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
-from ctfapp.utils.time import before_start
+from ctfapp.utils.time import before_start, after_end
 
 from functools import wraps
 
@@ -31,6 +31,16 @@ def lock_before_contest(view):
     @wraps(view, assigned=available_attrs(view))
     def wrap(request, *args, **kwargs):
         if before_start() and not request.user.is_staff:
+            return render(request, 'denied.html', {})
+        else:
+            return view(request, *args, **kwargs)
+
+    return wrap
+
+def lock_after_contest(view):
+    @wraps(view, assigned=available_attrs(view))
+    def wrap(request, *args, **kwargs):
+        if after_end() and not request.user.is_staff:
             return render(request, 'denied.html', {})
         else:
             return view(request, *args, **kwargs)
