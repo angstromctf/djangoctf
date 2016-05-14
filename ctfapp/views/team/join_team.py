b31e39f9766c42f241836fd7f9c0fb5fe521e6e0
Noah Singer <singerng@gmail.com>
2016-05-13 22:10:03 -0400

    Started cleaning up
    Moved some files around and deleted others

--- ctfapp/views/team/join_team.py
+++ ctfapp/views/team/join_team.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.core.urlresolvers import reverse
 
 from ctfapp.forms import ChangePasswordForm, CreateTeamForm, JoinTeamForm
 from ctfapp.models import Team
@@ -28,7 +29,7 @@ def join_team(request: HttpRequest):
         request.user.userprofile.team = team
         request.user.userprofile.save()
 
-        return redirect('/account/')
+        return reverse('account')
 
     return render(request, 'account.html', {'user': request.user,
                                             'change_password': ChangePasswordForm(user=request.user),
