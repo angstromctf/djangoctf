cff297a76754cf65161ba7774d146fddd508da1e
Anthony Li <tongning05@gmail.com>
2016-05-25 10:22:05 -0400

    Fix url references after cleanup

--- ctfapp/views/__init__.py
+++ ctfapp/views/__init__.py
@@ -1,15 +1,13 @@
-from ctfapp.views.problems.submit_problem import submit_problem
-from ctfapp.views.team.change_password import change_password
-from ctfapp.views.team.join_team import join_team
-from ctfapp.views.users.activation import activation, new_activation_link
-from ctfapp.views.users.score import score
-from ctfapp.views.users.signup import signup
+
+
 from .about import about
 from .chat import chat
 from .index import index
 from .learn import learn
-from .problems import problems
 from .scoreboard import scoreboard
 from .shelld import shelld
 from .updates import updates
 from .sponsors import sponsors
+from ctfapp.views import team
+from ctfapp.views import users
+from ctfapp.views import problems
