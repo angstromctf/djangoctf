c18305470bebc4dd1fbe064036deb3b10e92ddf6
Anthony Li <tongning05@gmail.com>
2016-05-29 00:57:55 -0400

    Bugfix

--- ctfapp/views/team/submit_addr.py
+++ ctfapp/views/team/submit_addr.py
@@ -31,9 +31,9 @@ def submit_addr(request: HttpRequest):
         user_team.address_zip = address_zip
         user_team.address_city = address_city
         user_team.address_state = address_state
-        user_team.eligible = form.cleaned_data['eligible']
+        user_team.eligible2 = form.cleaned_data['eligible2']
         user = request.user.userprofile
-        user.eligible = form.cleaned_data['eligible']
+        user.eligible = form.cleaned_data['eligible2']
         user.save()
         user_team.save()
     else:
