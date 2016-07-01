d8428e49a9db82fadb69771c545ddd2d6778b629
Noah Singer <singerng@gmail.com>
2016-07-01 00:47:02 -0400

    Scoreboard stops when contest ends

--- ctfapp/views/scoreboard.py
+++ ctfapp/views/scoreboard.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from ctfapp.models import Team, CorrectSubmission
-from ctfapp.utils.time import contest_start, minutes
+from ctfapp.utils.time import contest_start, contest_end, minutes
 
 def scoreboard(request: HttpRequest):
     """
@@ -12,7 +12,7 @@ def scoreboard(request: HttpRequest):
     team_list = Team.objects.filter(score__gt=0).order_by('-score', 'score_lastupdate')
 
     solutions_list = []
-    graph_size = min(5, Team.objects.all().count())
+    graph_size = min(5, Team.objects.filter(score__gt=0).count())
 
     for x in range(graph_size):
         submissions = CorrectSubmission.objects.all().filter(team=team_list[x])
@@ -22,7 +22,7 @@ def scoreboard(request: HttpRequest):
 
             solutions_list.append([minutes(delta)] + [-1] * x + [sub.new_score] + [-1] * (graph_size-1-x))
 
-    delta = timezone.now() - contest_start
+    delta = min(timezone.now(), contest_end) - contest_start
     solutions_list.append([minutes(delta)] + [team_list[x].score for x in range(graph_size)])
     
     solutions_list.sort()
