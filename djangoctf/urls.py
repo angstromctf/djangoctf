6ca434e594458f430912bd42368dba0f2f45f008
Noah Kim <noahbkim@gmail.com>
2017-07-25 20:22:36 -0400

    Remove swagger.

--- djangoctf/urls.py
+++ djangoctf/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 
 from rest_framework.routers import DefaultRouter
-from rest_framework_swagger.views import get_swagger_view
 
 from api import views
 
@@ -11,12 +10,10 @@ router.register('problems', views.ProblemViewSet)
 router.register('teams', views.TeamViewSet)
 router.register('users', views.UserViewSet)
 
-schema_view = get_swagger_view(title='djangoctf API')
-
 urlpatterns = [
-    url('^api/$', schema_view),
     url('^api/schema', views.schema),
     url(r'^api/', include(router.urls)),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
 urlpatterns += router.urls
